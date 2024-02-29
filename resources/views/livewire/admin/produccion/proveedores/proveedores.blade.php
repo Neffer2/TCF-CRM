8c47cd6c1fb09f35b31ee97935b46363b91d2007
Neffer <n3f73r@gmail.com>
2024-02-29 14:41:16 -0500

    Orden alfabetico proveedores

--- resources/views/livewire/admin/produccion/proveedores/proveedores.blade.php
+++ resources/views/livewire/admin/produccion/proveedores/proveedores.blade.php
@@ -47,87 +47,87 @@
             </div>
         </div>
         <div class="card-body p-0 pt-1">
-            <table class="table">
-                <thead> 
-                    <th colspan="6" class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">Informaci&oacute;n proveedores</th>
-                </thead>  
-                <tbody>  
-                    @foreach ($proveedores as $proveedor)
-                        <tr> 
-                            <td>
-                                <div class="d-flex px-2 py-1">
-                                    <div>
-                                        <img src="https://www.bullmarketing.com.co/wp-content/uploads/2022/04/cropped-favicon-bull-192x192.png" class="avatar avatar-sm me-3">
+            <div class="table-responsive">
+                <table class="table">
+                    <thead> 
+                        <th colspan="6" class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">Informaci&oacute;n proveedores</th>
+                    </thead>  
+                    <tbody>  
+                        @foreach ($proveedores as $proveedor)
+                            <tr> 
+                                <td>
+                                    <div class="d-flex px-2 py-1">
+                                        <div>
+                                            <img src="https://www.bullmarketing.com.co/wp-content/uploads/2022/04/cropped-favicon-bull-192x192.png" class="avatar avatar-sm me-3">
+                                        </div>
+                                        <div class="d-flex flex-column justify-content-center">
+                                            <h6 class="mb-0 text-xs">{{ $proveedor->tercero }}</h6>
+                                            <p class="text-xs text-secondary mb-0">{{ $proveedor->contacto }}</p>
+                                        </div>
                                     </div>
-                                    <div class="d-flex flex-column justify-content-center">
-                                        <h6 class="mb-0 text-xs">{{ $proveedor->tercero }}</h6>
-                                        <p class="text-xs text-secondary mb-0">{{ $proveedor->contacto }}</p>
+                                </td>
+                                <td>
+                                    <p class="text-xs font-weight-bold mb-0">Tipo</p> 
+                                    <span class="badge badge-sm badge-primary">{{ $proveedor->tipo }}</span>
+                                </td>
+                                <td>
+                                    <p class="text-xs font-weight-bold mb-0">{{ $proveedor->tipo_doc }}</p>
+                                    <p class="text-xs text-secondary mb-0">{{ $proveedor->documento }}</p>
+                                </td>
+                                <td>
+                                    <p class="text-xs font-weight-bold mb-0">{{ $proveedor->correo }}</p>
+                                    <p class="text-xs text-secondary">{{ $proveedor->celular }}</p>
+                                </td>
+                                <td>
+                                    <p class="text-xs font-weight-bold mb-0">{{ $proveedor->departamento }}</p>
+                                    <p class="text-xs text-secondary">{{ $proveedor->ciudad }}</p>
+                                </td>
+                                <td colspan="2"> 
+                                    <button @if (!(Auth::user()->rol == 1)) disabled @endif class="btn bg-gradient-primary mb-0" data-bs-toggle="modal" data-bs-target="#editModal{{ $proveedor->id }}">Editar</button>
+                                    <button @if (!(Auth::user()->rol == 1)) disabled @endif class="btn bg-gradient-danger mb-0" data-bs-toggle="modal" data-bs-target="#deleteModal{{ $proveedor->id }}">Eliminar</button>
+                                </td>
+                            </tr>
+                            <!-- Edit Modal --> 
+                            <div class="modal fade" id="editModal{{ $proveedor->id }}" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                                <div class="modal-dialog modal-dialog-centered modal-lg" role="document">
+                                    <div class="modal-content">
+                                        <div class="modal-header"> 
+                                            <h5 class="modal-title" id="exampleModalLabel">{{ $proveedor->contacto }}</h5>
+                                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close">
+                                                <span aria-hidden="true">&times;</span>
+                                            </button>
+                                        </div>
+                                        <div class="modal-body">
+                                            @livewire('admin.produccion.proveedores.nuevo-proveedor', ['proveedor_id' => $proveedor->id], key($proveedor->id))
+                                        </div>
                                     </div>
-                                </div>
-                            </td>
-                            <td>
-                                <p class="text-xs font-weight-bold mb-0">Tipo</p> 
-                                <span class="badge badge-sm badge-primary">{{ $proveedor->tipo }}</span>
-                            </td>
-                            <td>
-                                <p class="text-xs font-weight-bold mb-0">{{ $proveedor->tipo_doc }}</p>
-                                <p class="text-xs text-secondary mb-0">{{ $proveedor->documento }}</p>
-                            </td>
-                            <td>
-                                <p class="text-xs font-weight-bold mb-0">{{ $proveedor->correo }}</p>
-                                <p class="text-xs text-secondary">{{ $proveedor->celular }}</p>
-                            </td>
-                            <td>
-                                <p class="text-xs font-weight-bold mb-0">{{ $proveedor->departamento }}</p>
-                                <p class="text-xs text-secondary">{{ $proveedor->ciudad }}</p>
-                            </td>
-                            <td colspan="2"> 
-                                <button @if (!(Auth::user()->rol == 1)) disabled @endif class="btn bg-gradient-primary mb-0" data-bs-toggle="modal" data-bs-target="#editModal{{ $proveedor->id }}">Editar</button>
-                                <button @if (!(Auth::user()->rol == 1)) disabled @endif class="btn bg-gradient-danger mb-0" data-bs-toggle="modal" data-bs-target="#deleteModal{{ $proveedor->id }}">Eliminar</button>
-                            </td>
-                        </tr>
-                        <!-- Edit Modal --> 
-                        <div class="modal fade" id="editModal{{ $proveedor->id }}" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                            <div class="modal-dialog modal-dialog-centered modal-lg" role="document">
-                                <div class="modal-content">
-                                    <div class="modal-header"> 
-                                        <h5 class="modal-title" id="exampleModalLabel">{{ $proveedor->contacto }}</h5>
-                                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close">
-                                            <span aria-hidden="true">&times;</span>
-                                        </button>
-                                    </div>
-                                    <div class="modal-body">
-                                        @livewire('admin.produccion.proveedores.nuevo-proveedor', ['proveedor_id' => $proveedor->id], key($proveedor->id))
-                                    </div>
-                                </div>
-                            </div> 
-                        </div>
-                        <!-- Delete Modal --> 
-                        <div class="modal fade" id="deleteModal{{ $proveedor->id }}" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                            <div class="modal-dialog modal-dialog-centered" role="document">
-                                <div class="modal-content">
-                                    <div class="modal-header"> 
-                                        <h5 class="modal-title" id="exampleModalLabel">{{ $proveedor->contacto }}</h5>
-                                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close">
-                                            <span aria-hidden="true">&times;</span>
-                                        </button>
-                                    </div>
-                                    <div class="modal-body">
-                                        ¿Deseas eliminar al proveedor: <b>{{ $proveedor->contacto }}</b>?
-                                    </div>
-                                    <div class="modal-footer">
-                                        <button type="button" class="btn bg-gradient-danger my-0" data-bs-dismiss="modal" wire:click="deleteProveedor({{ $proveedor->id }})">Eliminar</button>
-                                        <button type="button" class="btn bg-gradient-secondary my-0" data-bs-dismiss="modal">Cerrar</button>
+                                </div> 
+                            </div>
+                            <!-- Delete Modal --> 
+                            <div class="modal fade" id="deleteModal{{ $proveedor->id }}" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                                <div class="modal-dialog modal-dialog-centered" role="document">
+                                    <div class="modal-content">
+                                        <div class="modal-header"> 
+                                            <h5 class="modal-title" id="exampleModalLabel">{{ $proveedor->contacto }}</h5>
+                                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close">
+                                                <span aria-hidden="true">&times;</span>
+                                            </button>
+                                        </div>
+                                        <div class="modal-body">
+                                            ¿Deseas eliminar al proveedor: <b>{{ $proveedor->contacto }}</b>?
+                                        </div>
+                                        <div class="modal-footer">
+                                            <button type="button" class="btn bg-gradient-danger my-0" data-bs-dismiss="modal" wire:click="deleteProveedor({{ $proveedor->id }})">Eliminar</button>
+                                            <button type="button" class="btn bg-gradient-secondary my-0" data-bs-dismiss="modal">Cerrar</button>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
-                        </div>
-                    @endforeach
-                    <tr>
-                        <td colspan="6" class="d-flex">{{ $proveedores->links() }}</td>
-                    </tr>
-                </tbody>
-            </table>
+                        @endforeach
+                    </tbody>
+                </table>
+                {{ $proveedores->links() }}
+            </div>
         </div>
     </div>
     <div class="alerts"> 
