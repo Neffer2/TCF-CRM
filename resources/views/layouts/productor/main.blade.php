a4dca000c3a79f44e4b11eaeed39536bec56b3d3
Neffer <n3f73r@gmail.com>
2024-06-27 09:36:31 -0500

    Actualizaciones listas

--- resources/views/layouts/productor/main.blade.php
+++ resources/views/layouts/productor/main.blade.php
@@ -99,7 +99,7 @@
             </ul>
           </div>
         </li>
-        <li class="nav-item">
+        {{-- <li class="nav-item">
           <a data-bs-toggle="collapse" href="#ajustes" class="nav-link" aria-controls="applicationsExamples" role="button" aria-expanded="false">
             <div class="icon icon-shape icon-sm text-center d-flex align-items-center justify-content-center">
               <i class="ni ni-box-2 text-success text-sm opacity-10"></i>
@@ -122,7 +122,7 @@
               </li>
             </ul>
           </div>
-        </li>
+        </li> --}}
         <li class="nav-item">
           <a data-bs-toggle="collapse" href="#ajustes" class="nav-link" aria-controls="applicationsExamples" role="button" aria-expanded="false">
             <div class="icon icon-shape icon-sm text-center d-flex align-items-center justify-content-center">
