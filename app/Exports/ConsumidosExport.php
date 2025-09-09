f0513da4f769758af473b5fe1c597b726f2fa851
Neffer2 <n3f73r@gmail.com>
2025-09-09 17:30:19 -0500

    1. Estados revisión en reporte condumidos

--- app/Exports/ConsumidosExport.php
+++ app/Exports/ConsumidosExport.php
@@ -19,18 +19,12 @@ class ConsumidosExport implements FromView, WithColumnFormatting, WithColumnWidt
     function __construct() {
         ini_set('max_execution_time', 10000); // or this way
 
-        $this->ordenes = OrdenCompra::where([
-            ['estado_id', '5'],
-            ['cod_causal', NULL]
-        ])
-        ->orWhere([
-            ['estado_id', '2'],
-            ['cod_causal', '!=', NULL]
-        ])
-        ->whereBetween('created_at', ['2025-01-01', Carbon::now()->toDateTimeString()])
-        // ->whereBetween('created_at', [Carbon::now()->startOfMonth()->toDateTimeString(), Carbon::now()->toDateTimeString()])
-        ->orderBy('created_at', 'desc')
-        ->get();
+        $this->ordenes = OrdenCompra::whereIn('estado_id', [5, 2])
+            ->where('cod_causal', NULL)
+            ->whereBetween('created_at', ['2025-01-01', Carbon::now()->toDateTimeString()])
+            // ->whereBetween('created_at', [Carbon::now()->startOfMonth()->toDateTimeString(), Carbon::now()->toDateTimeString()])
+            ->orderBy('created_at', 'desc')
+            ->get();
     }
 
     /**
