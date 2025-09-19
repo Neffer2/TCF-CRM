eef22e8842363b2f6c576477f1bfeabaa81c6555
Neffer2 <n3f73r@gmail.com>
2025-09-19 12:42:27 -0500

    1. consecutivo de factura visible

--- resources/views/livewire/admin/generales/helisa-general.blade.php
+++ resources/views/livewire/admin/generales/helisa-general.blade.php
@@ -1,7 +1,7 @@
 <div>
     <div class="card">
         <div class="card-header p-0 px-3 mt-3 position-relative z-index-1 col-md-12">
-            <div class="row">            
+            <div class="row">
                 <div class="col-md-12">
                     <h3 class="mb-0">Presupuestos</h3>
                     <p class="text-sm mb-0">Lista completa de presupuestos.</p>
@@ -16,11 +16,11 @@
                     </select>
                 </div>
                 <div class="form-group col-md-2">
-                    <label for="comercial">Buscar:</label> 
+                    <label for="comercial">Buscar:</label>
                     <input type="text" wire:model="centro" class="form-control" placeholder="Centro de costos">
                 </div>
                 <div class="form-group col-md-2">
-                    <label for="comercial">Comercial:</label> 
+                    <label for="comercial">Comercial:</label>
                     <select wire:model="comercial" class="form-control">
                         <option value="">Seleccionar</option>
                         @foreach ($comerciales as $comercial)
@@ -28,17 +28,17 @@
                         @endforeach
                     </select>
                 </div>
-            </div> 
-        </div>  
-        <div class="table-responsive"> 
+            </div>
+        </div>
+        <div class="table-responsive">
             <table class="table align-items-center mb-0">
-                <thead> 
+                <thead>
                     <tr>
                         <th colspan="1" class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">DATOS DE PROYECTO</th>
                         <th colspan="6" class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">M&eacute;tricas</th>
                     </tr>
                 </thead>
-                <tbody> 
+                <tbody>
                     @foreach ($registros_helisa as $registro_helisa)
                         <tr>
                             <td style="width: 16rem;">
@@ -51,15 +51,19 @@
                                         <p class="text-xs text-secondary mb-0">{{ $registro_helisa->nom_centro_costo }}</p>
                                     </div>
                                 </div>
-                            </td>                            
+                            </td>
                             <td>
                                 <p class="text-xs font-weight-bold mb-0">Fecha</p>
                                 <p class="text-xs text-secondary mb-0">{{ $registro_helisa->fecha }}</p>
-                            </td> 
+                            </td>
                             <td>
                                 <p class="text-xs font-weight-bold mb-0">Comercial</p>
                                 <p class="text-xs text-secondary mb-0">{{ $registro_helisa->comercial_user->name }}</p>
-                            </td>  
+                            </td>
+                            <td>
+                                <p class="text-xs font-weight-bold mb-0">FACTURA</p>
+                                <p class="text-xs text-secondary mb-0">{{ $registro_helisa->num_doc }}</p>
+                            </td>
                             <td>
                                 <p class="text-xs font-weight-bold mb-0">Centro de costos</p>
                                 <textarea disabled rows="1" class="text-xs text-secondary mb-0">{{ $registro_helisa->centro }}</textarea>
@@ -67,7 +71,7 @@
                             <td>
                                 <p class="text-xs font-weight-bold mb-0">Debito</p>
                                 <p class="text-xs text-secondary mb-0">$ {{ number_format($registro_helisa->debito) }}</p>
-                            </td> 
+                            </td>
                             <td>
                                 <p class="text-xs font-weight-bold mb-0">Cr&eacute;dito</p>
                                 <p class="text-xs text-secondary mb-0">$ {{ number_format($registro_helisa->credito) }}</p>
@@ -76,7 +80,7 @@
                                 <p class="text-xs font-weight-bold mb-0">Base factura</p>
                                 <p class="text-xs text-secondary mb-0 font-weight-bold">$ {{ number_format($registro_helisa->base_factura) }}</p>
                             </td>
-                        </tr> 
+                        </tr>
                     @endforeach
                 </tbody>
             </table>
@@ -88,7 +92,7 @@
                     $registros_page = sizeof($registrosHelisaArray['data']);
                     $total = $registrosHelisaArray['total'];
                 @endphp
-                <span class="text-xs text-secondary mb-0">Mostrando {{ $registros_page }} registros de {{ $total }}.</span>        
+                <span class="text-xs text-secondary mb-0">Mostrando {{ $registros_page }} registros de {{ $total }}.</span>
             </div>
             <div class="col-md-12 table-responsive">
                 {{ $registros_helisa->links() }}
@@ -96,7 +100,7 @@
         </div>
         <div class="row">
             <div class="col-md-6">
-                <button class="btn bg-gradient-warning m-2" wire:click="exportar">Exportar</button> 
+                <button class="btn bg-gradient-warning m-2" wire:click="exportar">Exportar</button>
             </div>
         </div>
     </div>
