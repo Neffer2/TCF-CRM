700799703111684213a875f44bdc43665622ddab
Neffer2 <n3f73r@gmail.com>
2026-02-24 09:32:05 -0500

    1. Fonts

--- resources/views/layouts/comercial/main.blade.php
+++ resources/views/layouts/comercial/main.blade.php
@@ -14,7 +14,7 @@
   <link href="{{ asset('assets/css/nucleo-icons.css') }}" rel="stylesheet" />
   <link href="{{ asset('assets/css/nucleo-svg.css') }}" rel="stylesheet" />
   <!-- Font Awesome Icons -->
-  <script src="https://kit.fontawesome.com/9c37d8ebcb.js" crossorigin="anonymous"></script>
+  <script src="https://kit.fontawesome.com/274ff3eb36.js" crossorigin="anonymous"></script>
   <link href="{{ asset('assets/css/nucleo-svg.css') }}" rel="stylesheet" />
   <!-- CSS Files -->
   <link id="pagestyle" href="{{ asset('assets/css/argon-dashboard.css?v=2.0.5') }}" rel="stylesheet" />
