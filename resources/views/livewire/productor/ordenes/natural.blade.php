e36e1ea7670690ba01136d2aacd847c9bb8ab17e
brandonvegadev <Brandon.Vega.dev@gmail.com>
2026-08-31 17:14:30 -0500

    Verificacion

--- resources/views/livewire/productor/ordenes/natural.blade.php
+++ resources/views/livewire/productor/ordenes/natural.blade.php
@@ -700,7 +700,7 @@
                                                 <td>
                                                     <p class="text-xs text-secondary mb-0">
                                                     @if($queriedOrden?->archivo_cuenta_cobro)
-                                                        <a href="{{ asset(str_replace("public", "storage", $queriedOrden->cuenta_cobro_url)) }}" target="_blank">Ver cuenta de cobro</a>
+                                                        <a href="{{ asset(str_replace("public", "storage", $queriedOrden->cuenta_cobro_url)) }}" target="_blank">Veeeer cuenta de cobro</a>
                                                     @else
                                                         Sin archivo
                                                     @endif
